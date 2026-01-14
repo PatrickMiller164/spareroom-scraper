afed37fbd45ed741172dea0be8bc5c49fce8f8ae
PatrickMiller164 <millerpatrickjames@gmail.com>
2026-01-14 20:09:47 +0000

    created config dataclass, simplified main.py

--- src/utils/types.py
+++ src/utils/types.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
+from collections import namedtuple
+
+coordinates = namedtuple('Location', ['latitude', 'longitude'], defaults=[None, None])
 
 @dataclass
 class Room:
@@ -8,31 +11,36 @@ class Room:
     url: str = ''
     id: Optional[str] = None
     date_added: date = date.today()
-    title: Optional[str] = None
+    
     type: Optional[str] = None
-    available_all_week: Optional[bool] = None
     area: Optional[str] = None
-    location: Optional[str] = None
     nearest_station: Optional[str] = None
+    available_all_week: Optional[bool] = None
+
     direct_line_to_office: Optional[bool] = None
+    location: Optional[str] = None
     location_1: Optional[str] = None
     location_2: Optional[str] = None
-    score: Optional[float] = None
+
     average_price: Optional[int] = None
     average_deposit: Optional[int] = None
+
     available: Optional[str] = None
     minimum_term: Optional[str] = None
     maximum_term: Optional[str] = None
-    bills_included: Optional[str] = None
-    broadband_included: Optional[str] = None
-    furnishings: Optional[str] = None
-    garden_or_patio: Optional[str] = None
-    living_room: Optional[str] = None
-    balcony_or_roof_terrace: Optional[str] = None
+
+    bills_included: Optional[bool] = None
+    broadband_included: Optional[bool] = None
+    furnishings: Optional[bool] = None
+    garden_or_patio: Optional[bool] = None
+    living_room: Optional[bool] = None
+    balcony_or_roof_terrace: Optional[bool] = None
+    
     number_of_flatmates: Optional[int] = None
     total_number_of_rooms: Optional[int] = None
     room_sizes: Optional[list] = None
-    image_url: Optional[str] = None
+    score: Optional[float] = None
+    image_url: str = ""
 
 
 @dataclass(frozen=True)
